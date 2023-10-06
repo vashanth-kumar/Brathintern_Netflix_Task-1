df4f0f6ee61254396c39df4b03997f99314d8b55
VashanthaKumar K S <120630583+vashanth-kumar@users.noreply.github.com>
2023-10-06 21:05:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// script.js
+
+// Get the "Get Started" button element
+const startButton = document.getElementById("start-button");
+
+// Add a click event listener to the button
+startButton.addEventListener("click", function () {
+    alert("Welcome to Netflix! Enjoy your streaming.");
+});
